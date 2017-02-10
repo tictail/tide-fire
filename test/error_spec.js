9001b4e9600a777befe07a69a9292c455033adf4
eldh <andreas.eldh@gmail.com>
2017-02-10 08:27:40 +0100

    Bind tide-fire to specific tide instances

--- test/error_spec.js
+++ test/error_spec.js
@@ -1,5 +1,5 @@
 import {test} from 'ava'
-import {fire, init, addActions} from '../src'
+import {init, addActions} from '../src'
 import {Base} from 'tide'
 import {Record, Map} from 'immutable'
 
@@ -21,9 +21,9 @@ init(tide, {
 })
 
 test('should throw when calling non-existing action', (t) => {
-  t.throws(() => fire('bar.getWine'))
-  t.throws(() => fire('gym.getBeer'))
-  t.throws(() => fire('notHere.atAll'), 'Can\'t find action object notHere')
+  t.throws(() => tide.fire('bar.getWine'))
+  t.throws(() => tide.fire('gym.getBeer'))
+  t.throws(() => tide.fire('notHere.atAll'), 'Can\'t find action object notHere')
 })
 
 test('should throw when adding non-action', (t) => {
@@ -36,12 +36,12 @@ test('should throw when initializing without a tide instance', (t) => {
 
 test('should throw when fire throws', (t) => {
   addActions({oops: {doh() { throw Error('Doh!') }}})
-  t.throws(() => fire('oops.doh'), 'Doh!')
+  t.throws(() => tide.fire('oops.doh'), 'Doh!')
 })
 
 test('should throw when action path is invalid', (t) => {
-  t.throws(() => fire('noDotCantDispatch'))
-  t.throws(() => fire('too.many.dots.cant.handle'))
+  t.throws(() => tide.fire('noDotCantDispatch'))
+  t.throws(() => tide.fire('too.many.dots.cant.handle'))
 })
 
 test('should pass on catch errors when action throws in promise', (t) => {
@@ -49,7 +49,7 @@ test('should pass on catch errors when action throws in promise', (t) => {
   addActions({oops: {
     pinky: () => new Promise(() => { throw Error('Doh!') }),
   }})
-  return fire('oops.pinky').catch((err) => {
+  return tide.fire('oops.pinky').catch((err) => {
     t.true(err instanceof Error)
     t.is(err.message, 'Error in fire(oops.pinky): Doh!')
   })
--- test/middleware_spec.js
+++ test/middleware_spec.js
@@ -1,5 +1,5 @@
 import {test} from 'ava'
-import {fire, init} from '../src'
+import {init} from '../src'
 import {Base} from 'tide'
 import {spy} from 'sinon'
 import {Record, Map} from 'immutable'
@@ -45,11 +45,11 @@ init(tide, {
 
 test('should use middleware to time execution of sync and async function', (t) => {
   t.plan(16)
-  fire('bar.getBeer').then((res) => t.is(res, 'Singha', 'action returns correct value'))
+  tide.fire('bar.getBeer').then((res) => t.is(res, 'Singha', 'action returns correct value'))
   t.true(middleSpy.calledOnce, 'Middleware is called')
   t.true(secondMiddleSpy.calledOnce, 'both middleware are called')
   t.is(middleSpy.lastCall.args[1], 'bar.getBeer', 'Middleware gets action name as 2nd argument')
-  fire('bar.getBeer', 'XXXX').then((res) => t.is(res, 'XXXX', 'action uses data'))
+  tide.fire('bar.getBeer', 'XXXX').then((res) => t.is(res, 'XXXX', 'action uses data'))
   t.deepEqual(actionSpy.lastCall.args[0], 'XXXX', 'action gets right arguments')
   t.true(typeof actionSpy.lastCall.args[1] === 'object', 'action gets right arguments')
   t.true(typeof actionSpy.lastCall.args[1].get === 'function', 'action gets right arguments')
@@ -58,7 +58,7 @@ test('should use middleware to time execution of sync and async function', (t) =
   t.true(middleSpy.calledTwice, 'middleware is called again')
   t.true(secondMiddleSpy.calledTwice, 'both middleware are called')
   t.true(logSpy.lastCall.args[0] >= 0, 'logging middeware works')
-  return fire('bar.getSlowBeer').then(() => {
+  return tide.fire('bar.getSlowBeer').then(() => {
     t.is(middleSpy.callCount, 3, 'async middeware gets called')
     t.is(secondMiddleSpy.callCount, 3, 'both middleware are called')
     t.true(logSpy.lastCall.args[0] >= 10, 'async middleware can time execution')
